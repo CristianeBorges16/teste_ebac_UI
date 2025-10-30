042b01cacf7a777e4e800ea57398eaae6bf80f09
CristianeBorges16 <160506853+CristianeBorges16@users.noreply.github.com>
2025-10-30 10:23:22 -0300

    Usando BeforeEach e AfterEach

--- cypress/e2e/3-loja-ebac/login.cy.js
+++ cypress/e2e/3-loja-ebac/login.cy.js
@@ -1,12 +1,35 @@
 /// < reference types="cypress"/>
 
 describe('Funcionalidade: Login', () => {
-    it('Deve fazer login com sucesso', () => {
+
+    beforeEach(() => {
         cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+    });
+
+    afterEach(() => {
+        cy.screenshot()
+    });
+
+    it('01-Deve fazer login com sucesso', () => {
         cy.get ('#username').type('cris.teste@teste.com.br')
         cy.get('#password').type('ebac-teste1!!!')
         cy.get('.woocommerce-form > .button').click()
-
         cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain' , 'Olá, cris.teste (não é cris.teste? Sair)')
+    });
+
+    it('02-Deve inserir uma mensagem de erro ao inserir usuário inválido', () => {
+        cy.get ('#username').type('invalido@teste.com.br')
+        cy.get('#password').type('ebac-teste1!!!')
+        cy.get('.woocommerce-form > .button').click()        
+        cy.get('.woocommerce-error > li').should('contain', 'Endereço de e-mail desconhecido')
+        cy.get('.woocommerce-error').should('exist')  // apenas verifica se este campo passa a existir quando ocorre erro de user
+    });
+
+        it('03-Deve inserir uma mensagem de erro ao inserir senha inválida', () => {
+        cy.get ('#username').type('cris.teste@teste.com.br')
+        cy.get('#password').type('teste1!!!')
+        cy.get('.woocommerce-form > .button').click()        
+        cy.get('.woocommerce-error > li').should('contain', 'A senha fornecida')
+        cy.get('.woocommerce-error').should('exist')  // apenas verifica se este campo passa a existir quando ocorre erro de user
     });
 });
